37d545c04246212dde728b144a998d033a3bc79d
kiryusensya <japanairline2018@gmail.com>
2026-04-15 12:20:25 +0900

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -9,6 +9,8 @@ function hashPassword(password) {
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
+const ADMIN_ID = "admin_kiryu-sensya";
+const ADMIN_PASS = "-GTA5Il-v4.2xs-2";
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
