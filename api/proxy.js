72ffb763ca14fb98e95e04d26b38bfaf085420eb
kiryusensya <japanairline2018@gmail.com>
2026-04-16 19:04:16 +0900

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
   res.setHeader('Pragma', 'no-cache');
   res.setHeader('Expires', '0');
