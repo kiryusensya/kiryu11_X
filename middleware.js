a1dfa7f740d011cc5916679f75ad5112aebc8f45
kiryusensya <japanairline2018@gmail.com>
2026-05-01 20:04:36 +0900

    Add auth parameter to redirect for unauthenticated users

--- middleware.js
+++ middleware.js
@@ -28,6 +28,7 @@ export default function middleware(request) {
         // Cookieの中に user_session_token が存在するかチェック
         if (!cookie.includes('user_session_token=')) {
             url.pathname = '/Access.html';
+            url.searchParams.set('auth', 'required'); // ★目印を追加
             return Response.redirect(url, 307);
         }
         return; // user_session_tokenがあれば通過
